16c62f59b8d59511481fe409151ac4410e907795
Daniel Botinelly <danielbotinelly@gmail.com>
2026-08-17 21:12:15 -0300

    replace System.Net.Mail with MailKit

--- StockQuoteAlert/Notifications/SmtpNotifier.cs
+++ StockQuoteAlert/Notifications/SmtpNotifier.cs
@@ -1,10 +1,13 @@
-using System.Net;
-using System.Net.Mail;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
 
 namespace StockQuoteAlert.Notifications;
 
 public class SmtpNotifier : INotifier
 {
+    private const int ImplicitTlsPort = 465;
+
     private readonly SmtpOptions _options;
 
     public SmtpNotifier(SmtpOptions options)
@@ -14,19 +17,29 @@ public class SmtpNotifier : INotifier
 
     public async Task SendAsync(Alert alert, CancellationToken cancellationToken)
     {
-        using var client = new SmtpClient(_options.Host, _options.Port)
+        var message = new MimeMessage
         {
-            EnableSsl = _options.EnableSsl
+            Subject = AlertEmailFormatter.Subject(alert),
+            Body = new TextPart("plain") { Text = AlertEmailFormatter.Body(alert) }
         };
+
+        message.From.Add(MailboxAddress.Parse(_options.From));
+        message.To.Add(MailboxAddress.Parse(_options.To));
+
+        var security = _options.EnableSsl
+            ? _options.Port == ImplicitTlsPort ? SecureSocketOptions.SslOnConnect : SecureSocketOptions.StartTls
+            : SecureSocketOptions.None;
+
+        using var client = new SmtpClient();
+
+        await client.ConnectAsync(_options.Host, _options.Port, security, cancellationToken);
 
         if (!string.IsNullOrWhiteSpace(_options.User))
         {
-            client.Credentials = new NetworkCredential(_options.User, _options.Password);
+            await client.AuthenticateAsync(_options.User, _options.Password, cancellationToken);
         }
 
-        using var message = new MailMessage(
-            _options.From, _options.To, AlertEmailFormatter.Subject(alert), AlertEmailFormatter.Body(alert));
-
-        await client.SendMailAsync(message, cancellationToken);
+        await client.SendAsync(message, cancellationToken);
+        await client.DisconnectAsync(quit: true, cancellationToken);
     }
 }
